372e23f794fc61e0e1ec38005807f1abc79384b0
kimsungheon <tjdgjswkd@naver.com>
2025-09-25 21:43:52 +0900

    feat: 15655

---
+++ algo/src/bj/bj_15655.java
@@ -0,0 +1,46 @@
+package bj;
+import java.util.*;
+import java.io.*;
+
+public class bj_15655 {
+    private static int n,m;
+    private static int[] num,arr;
+    private static boolean[] visited;
+    private static StringBuilder sb=new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        n=Integer.parseInt(st.nextToken());
+        m=Integer.parseInt(st.nextToken());
+
+        arr=new int[n];
+        num=new int[m];
+        visited=new boolean[n];
+
+        st=new StringTokenizer(br.readLine());
+
+        for (int i = 0; i <n ; i++) {
+            arr[i]=Integer.parseInt(st.nextToken());
+        }
+        //사전순
+        Arrays.sort(arr);
+
+        dfs(0,0);
+        System.out.println(sb);
+    }
+    private static void dfs(int depth,int start){
+        if(depth==m){
+            for (int i = 0; i < m; i++) {
+                sb.append(num[i]).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+        for (int i = start; i <n ; i++) {
+
+                num[depth]=arr[i];
+                dfs(depth+1,i+1);
+
+        }
+    }
+}
