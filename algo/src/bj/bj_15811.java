b4b01d8d96cbdbefe0f20e24ef4ea25e53a9f10d
김성헌 <tjdgjswkd@naver.com>
2025-10-20 18:28:22 +0900

    feat: 15811

--- algo/src/bj/bj_15811.java
+++ algo/src/bj/bj_15811.java
@@ -4,79 +4,93 @@ import java.util.*;
 
 public class bj_15811 {
     private static List<Character> alp;
-    private static Map<Character,Integer> map=new HashMap<>();
-    private static String a,b,c;
+    private static int[] alphaNum = new int[26]; // 'A'~'Z'에 해당하는 숫자 저장
+    private static String a, b, c;
     private static int n;
-    private static boolean[] visited=new boolean[10];
-    private static boolean flag=false;
-    public static void main(String[] args) throws IOException{
-        BufferedReader br=new  BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st=new StringTokenizer(br.readLine());
+    private static boolean[] visited = new boolean[10];
+    private static boolean flag = false;
+    private static Set<Character> firstChars = new HashSet<>();
 
-        a=st.nextToken();
-        b=st.nextToken();
-        c=st.nextToken();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        Set<Character> set=new HashSet<>();
+        a = st.nextToken();
+        b = st.nextToken();
+        c = st.nextToken();
 
-        for (int i = 0; i < a.length(); i++) {
-            char tmp=a.charAt(i);
-            set.add(tmp);
+        int maxLen=Math.max(a.length(),b.length());
+
+        if (c.length() < maxLen || c.length() > maxLen + 1) {
+            System.out.println("NO");
+            return;
         }
-        for (int i = 0; i < b.length(); i++) {
-            char tmp=b.charAt(i);
-            set.add(tmp);
-        }
-        for (int i = 0; i < c.length(); i++) {
-            char tmp=c.charAt(i);
-            set.add(tmp);
-        }
-        alp=new ArrayList<>(set);
-        n=alp.size();
 
-        //dfs로 알파벳 숫자 할당해서 조합 만들기
+        firstChars.add(a.charAt(0));
+        firstChars.add(b.charAt(0));
+        firstChars.add(c.charAt(0));
+
+        Set<Character> set = new HashSet<>();
+        for (char tmp : a.toCharArray()) set.add(tmp);
+        for (char tmp : b.toCharArray()) set.add(tmp);
+        for (char tmp : c.toCharArray()) set.add(tmp);
+
+        alp = new ArrayList<>(set);
+        n = alp.size();
+
+        if (n > 10) {
+            System.out.println("NO");
+            return;
+        }
+
         dfs(0);
-        //숫자로 바꿔서 계산하는데 맨 앞자리 0되면 안됨
-        if(!flag) System.out.println("NO");
 
-
+        if (!flag) {
+            System.out.println("NO");
+        }
     }
-    private static void dfs(int depth){
-        if(flag) return; // 이미 답 찾은 상태
-        if(depth==n){
-            //맨앞자리 0일때
-            if(map.get(a.charAt(0))==0) return;
-            if(map.get(b.charAt(0))==0) return;
-            if(map.get(c.charAt(0))==0) return;
 
-            long numA=toNum(a);
-            long numB=toNum(b);
-            long numC=toNum(c);
+    private static void dfs(int depth) {
+        if (flag) {
+            return;
+        }
 
-            if(numA+numB==numC){
+        if (depth == n) {
+            long numA = toNum(a);
+            long numB = toNum(b);
+            long numC = toNum(c);
+
+            if (numA + numB == numC) {
                 System.out.println("YES");
-                flag=true;
+                flag = true;
             }
             return;
-
         }
 
-        char target=alp.get(depth);
-        for (int i = 0; i <=9 ; i++) {
-            if(!visited[i]){
-                //i라는 숫자를 아직 사용하지 않았다면
-                visited[i]=true;
-                map.put(target,i);
-                dfs(depth+1);
-                visited[i]=false;
+        char target = alp.get(depth);
+        for (int i = 0; i <= 9; i++) {
+            if (firstChars.contains(target) && i == 0) {
+                continue;
+            }
+
+            if (!visited[i]) {
+                visited[i] = true;
+
+                alphaNum[target - 'A'] = i;
+
+                dfs(depth + 1);
+
+                visited[i] = false;
+
             }
         }
-
     }
-    private static long toNum(String s){
-        long value=0;
-        for (int i = 0; i < s.length(); i++) {
-            value=value*10+map.get(s.charAt(i));
+
+    private static long toNum(String s) {
+        long value = 0;
+        for (char ch : s.toCharArray()) {
+
+            value = value * 10 + alphaNum[ch - 'A'];
         }
         return value;
     }
